64e2c500b1946edeb5f1a029aab52264b6cf4da7
Chandan U <chandanucodeid04@gmail.com>
2025-09-15 12:56:13 +0530

    Create pub.py

---
+++ 10/pub.py
@@ -0,0 +1,10 @@
+import paho.mqtt.client as mqtt
+
+broker = "test.mosquitto.org"
+topic = "test/simplemqtt"
+
+pub = mqtt.Client(client_id="pub")
+pub.connect(broker)
+pub.publish(topic, "Hello MQTT!")
+print("Published: Hello MQTT!")
+pub.disconnect()
